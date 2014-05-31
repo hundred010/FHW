17dfebbe46bcfc7528b59b9e6525253b1d463cc6
hundred010 <ji-ye@Ji-Yeteki-MacBook-Pro.local>
2014-05-31 09:52:25 +0800

    FHW

---
+++ FHW/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  FHW
+//
+//  Created by hundred010 on 14/5/31.
+//  Copyright (c) 2014年 hundred010. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)B1:(id)sender
+{
+    _Label.text=@"200417052";
+}
+
+- (IBAction)B2:(id)sender
+{
+    _Label.text=@"陳彥百";
+}
+@end
